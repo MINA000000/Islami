fd4ddda93d0c61b5c1d73a09c8e0651de5f0fb12
MINA000000 <minanasserwanaszero@gmail.com>
2024-10-04 15:53:44 +0300

    initiate navigation bar

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const routeName = '/home';
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xffB7935F),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/radio.png'),
+              color: _selectedIndex == 0 ? Colors.black : Colors.white,
+            ),
+            label: 'Radio',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/sebha.png'),
+              color: _selectedIndex == 1 ? Colors.black : Colors.white,
+            ),
+            label: 'Sebha',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/quran_quran_svgrepo_com.png'),
+              color: _selectedIndex == 2 ? Colors.black : Colors.white,
+            ),
+            label: 'Quran',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage('assets/images/quran.png'),
+              color: _selectedIndex == 3 ? Colors.black : Colors.white,
+            ),
+            label: 'Quran 2',
+          ),
+        ],
+      ),
+    );
   }
 }
